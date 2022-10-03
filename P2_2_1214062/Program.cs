d2a28c4996b931faa3b7e03b906490f1ed89248c
Dzikri718 <izzatulhaq8@gmail.com>
2022-10-03 12:22:57 +0700

    perubahan

--- P2_2_1214062/Program.cs
+++ P2_2_1214062/Program.cs
@@ -13,15 +13,15 @@ namespace P2_2_1214062
 
             Console.WriteLine("== Aplikasi Penghitung Gaji Karyawan ==");
             Console.Write("Masukkan Nominal Gaji Anda : ");
-            var gaji = int.Parse(Console.ReadLine());
+            var gaji_pokok = int.Parse(Console.ReadLine());
 
-            double tunjangan = 20.0 / 100.0 * gaji;
-            double bonus = 15.0 / 100.0 * gaji;
-            double pph = 3.5 / 100.0 * gaji;
-            double total_gaji = gaji + tunjangan + bonus;
+            double tunjangan = 20.0 / 100.0 * gaji_pokok;
+            double bonus = 15.0 / 100.0 * gaji_pokok;
+            double pph = 3.5 / 100.0 * gaji_pokok;
+            double total_gaji = gaji_pokok + tunjangan + bonus;
             double gaji_bersih = total_gaji - pph;
 
-            Console.WriteLine("Gaji Pokok    =   " + gaji);
+            Console.WriteLine("Gaji Pokok    =   " + gaji_pokok);
             Console.WriteLine("Tunjangan     =   " + tunjangan);
             Console.WriteLine("Bonus         =   " + bonus);
             Console.WriteLine("PPH           =   " + pph);
